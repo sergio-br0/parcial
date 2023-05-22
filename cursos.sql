1f85ad6ab8fd257cebef3afa9e855ab4b95b2c84
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-05-22 08:01:19 -0600

    tabla para la base de datos

---
+++ cursos.sql
@@ -0,0 +1,8 @@
+CREATE TABLE cursos (
+    cursos_id SERIAL PRIMARY KEY NOT NULL ,
+    cursos_nombre VARCHAR(50) NOT NULL,
+    cursos_cantidad INT NOT NULL,
+    cursos_fase VARCHAR (50) NOT NULL,
+    cursos_instructor VARCHAR(50) NOT NULL,
+    cursos_creditos INT NOT NULL
+);
