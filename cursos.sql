19e032961574f07a7cba3f713ef570fdd23d240f
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-05-22 08:08:01 -0600

    conexion con la base de datos

--- cursos.sql
+++ cursos.sql
@@ -1,7 +1,7 @@
 CREATE TABLE cursos (
     cursos_id SERIAL PRIMARY KEY NOT NULL ,
     cursos_nombre VARCHAR(50) NOT NULL,
-    cursos_cantidad INT NOT NULL,
+    cursos_cantidad_per INT NOT NULL,
     cursos_fase VARCHAR (50) NOT NULL,
     cursos_instructor VARCHAR(50) NOT NULL,
     cursos_creditos INT NOT NULL
---
+++ modelos/conexion.php
@@ -0,0 +1,28 @@
+<?php
+require 'Conexion.php';
+
+class cursos extends Conexion{
+    public $cursos_id;
+    public $cursos_nombre;
+    public $cursos_cantidad_per;
+    public $cursos_fase;
+    public $cursos_instructor;
+    public $cursos_creditos;
+
+
+    public function __construct($args = [] )
+    {
+        $this->cursos_id = $args['cursos_id'] ?? null;
+        $this->cursos_nombre = $args['cursos_nombre'] ?? '';
+        $this->cursos_cantidad_per = $args['cursos_cantidad_per'] ?? '';
+        $this->cursos_fase = $args['cursos_fase'] ?? '';
+        $this->cursos_instructor = $args['cursos_instructor'] ?? '';
+        $this->cursos_creditos = $args['cursos_creditos'] ?? '';
+    }
+
+    public function guardar(){
+        $sql = "INSERT INTO cursos(cursos_nombre, cursos_cantidad_per, cursos_fase, cursos_instructor, cursos_creditos) values('$this->cursos_nombre','$this->cursos_cantidad_per', '$this->cursos_fase', '$this->cursos_instructor', '$this->cursos_creditos')";
+        $resultado = self::ejecutar($sql);
+        return $resultado;
+    }
+}
