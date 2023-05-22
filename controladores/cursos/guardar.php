0069c7d1d9106b5811345033c02f61d25720d2e5
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-05-22 08:35:42 -0600

    informacion guardada en la base de datos exitosa...

--- controladores/cursos/guardar.php
+++ controladores/cursos/guardar.php
@@ -27,7 +27,7 @@ try {
             <div class="col-lg-6">
                 <?php if($resultado): ?>
                     <div class="alert alert-success" role="alert">
-                        Guardado exitosamente!
+                        Informacion guardada exitosamente!
                     </div>
                 <?php else :?>
                     <div class="alert alert-danger" role="alert">
