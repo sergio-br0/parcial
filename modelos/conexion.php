ec772f3b6db5e17cb5f58dd422137730925f3c30
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-05-22 08:12:25 -0600

    clase conexion para conectar con la base de datos

---
+++ modelos/conexion.php
@@ -0,0 +1,50 @@
+<?php
+
+abstract class Conexion{
+    public static $conexion = null;
+
+    private static function conectar(){
+        try{
+            //CONEXION A LA BD DE INFORMIX EN DOCKER 
+            self::$conexion = new PDO('informix:host=host.docker.internal; service=9088; database=mdn; server=informix; protocol=onsoctcp; EnableScrollableCursors = 1','informix','in4mix'); 
+            // DEFINIR EL MANEJO DE EXCEPCIONES
+            self::$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+            // IMPRIME EN PANTALLA EL ERROR
+            echo "Error de conexion de BD";
+            echo "<br>";
+            echo $e->getMessage();
+            exit;
+        }
+
+        return self::$conexion;
+    }
+
+    public static function ejecutar($sql){
+        // CONECTANDOSE A LA BD CON EL METODO CONECTAR
+        self::conectar();
+        // PREPARAMOS LA SENTENCIA
+        $sentencia = self::$conexion->prepare($sql);
+        // EJECUTAMOS A SENTENCIA
+        $resultado = $sentencia->execute();
+        // CERRANDO LA CONEXION
+        self::$conexion = null;
+        // DEVOLVEMOS RESULTADOS
+        return $resultado;
+    }
+
+    public static function servir($sql){
+        // CONECTANDOSE A LA BD CON EL METODO CONECTAR
+        self::conectar();
+        // PREPARAMOS LA SENTENCIA
+        $sentencia = self::$conexion->prepare($sql);
+        // EJECUTAMOS A SENTENCIA
+        $sentencia->execute();
+        $resultados = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+
+        // CERRANDO LA CONEXION
+        self::$conexion = null;
+        // DEVOLVEMOS RESULTADOS
+        return $resultados;
+    }
+}
