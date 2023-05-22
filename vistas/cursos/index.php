2b6b9f886126becde0fb69bd972d6b06521ceca9
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-05-22 08:38:19 -0600

    cambio de color al titulo del formulario

--- vistas/cursos/index.php
+++ vistas/cursos/index.php
@@ -6,6 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Info_cursos</title>
+    <style>
+        .text-center{
+            color: blue;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
